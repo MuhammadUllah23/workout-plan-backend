4e320e46d398f868ca9c5f79ba91d2583acbd74d
Muhammad <muhammadhullah23@gmail.com>
2021-12-07 18:59:56 -0500

    User routes can create a new user, show a user, and show all users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :sets_and_reps
   resources :exercises
   resources :workout_sessions
-  resources :users
+  resources :users, only: [:create, :show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
