0255d8e44b7a2ee173e351bf72bdc8ef2fdc1ec1
Muhammad <muhammadhullah23@gmail.com>
2021-12-07 21:25:47 -0500

    change ActionController::API to ActionController::Base to get full functionality of a Rails controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,4 +27,10 @@ class UsersController < ApplicationController
             }
         end
     end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
