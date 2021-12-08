0290db4dccc4aad169b00387f08bc7dbddca3d24
Muhammad <muhammadhullah23@gmail.com>
2021-12-07 21:11:03 -0500

    create User create action to create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,5 +16,15 @@ class UsersController < ApplicationController
         end
     end
 
-    
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else 
+            render json: {
+                status: 500,
+                errors: user.errors.full_messages
+            }
+        end
+    end
 end
